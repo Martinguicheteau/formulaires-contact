f8f22f361df4961441ef9a62494b1ed65e349d5a
aloonska <45172544+aloonska@users.noreply.github.com>
2018-12-06 13:57:15 +0100

    Index

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+  <html>
+  <head>
+  <meta charset="utf-8">
+  <title>Formulaire de contact </title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+<br/><br/><br/><br/>
+<center>
+<a href="login.php" title="Life Is Strange 1" > <button type='submit' class='btn btn-dark'>Se Connecter</button></a>
+
+<a href="register.php" title="Life Is Strange 1" > <button type='submit' class='btn btn-dark'>S'inscrire</button></a>
+</center>
+  </body>
+  </html>
+  <?php
