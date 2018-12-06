bb33acb5f2111ceb8d5e5e08ef032e40db75d0ce
Martin Guicheteau <44553831+Martinguicheteau@users.noreply.github.com>
2018-12-06 15:59:03 +0100

    voila

---
+++ Config.php
@@ -0,0 +1,10 @@
+<?php
+class Config
+{
+//    public static $HOST = '172.17.0.3';
+    const HOST = 'localhost'; // localhost
+    const PORT = 3306;
+    const DATABASE = 'formulaire_contact';
+    const LOGIN = 'root';
+    const PASSWORD = '';
+}
--- dashboard.php
+++ dashboard.php
@@ -1,17 +1,77 @@
-<?php 
-session_start();
+<?php
+require_once 'Config.php';
+require_once 'Book.php';
+ $host = 'localhost'; 
+$port = 3306;
+$database = 'formulaire_contacte';
+ $books = array();
+
+ try {
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$database",
+        'root',
+        '');
+    $driver = sprintf(
+        "mysql:host=%s;port=%s;dbname=%s",
+        Config::HOST,
+        Config::PORT,
+        Config::DATABASE
+        );
+     $pdo = new PDO(
+        $driver,
+        Config::LOGIN,
+        Config::PASSWORD);
+     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     $pdo->exec("INSERT INTO books (name) VALUES ('famille');");
+    //$pdo->exec("INSERT INTO clients (firstname, lastname) VALUES ('jane', 'die');");
+      var_dump("Le dernier ID est : " . $pdo->lastInsertId());
+     $stmt = $pdo->query("SELECT * FROM book;");
+ 
+ //    var_dump($stmt->fetchObject());
+ 
+     while (($row = $stmt->fetch(PDO::FETCH_ASSOC)) !== false) {
+         $books = new Book(
+             $row['id'],
+             $row['name']
+         );
+ 
+        $books[] = $book;
+    }
+     var_dump($books);
+ //    var_dump($stmt);
+} catch (PDOException $e) {
+    var_dump($e->getMessage());
+//    var_dump("Bad credentials");
+} finally {
+    $pdo = null;
+}
 ?>
-<html lang="fr">
-<link rel="stylesheet" href="../CSS/styles.css"/>
+
+<!doctype html>
+<html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>formulaire contacte  </title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>formulaire contacte</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
-<body> <center>
-	<br><br>
-<h1> Bienvenue sur votre DASHBORD </h1>
-<button type='submit' class='btn btn-dark'>Crée un BOOK !</button></a>
-</center>
+<body>
+<table class="table">
+    <thead>
+    <tr>
+        <th>#</th>
+        <th>Firstname</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($books as $book): ?>
+    <tr>
+        <td><?= $client->getId() ?></td>
+        <td><?= $client->getFirstname() ?></td>
+    </tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
 </body>
-</html>
+</html> 
