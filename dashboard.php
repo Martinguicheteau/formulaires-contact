1c6f5261a2d37e11efe8ef3815c23be9755fb7c7
aloonska <45172544+aloonska@users.noreply.github.com>
2018-12-06 15:32:54 +0100

    Dashboard interface

---
+++ dashboard.php
@@ -0,0 +1,17 @@
+<?php 
+session_start();
+?>
+<html lang="fr">
+<link rel="stylesheet" href="../CSS/styles.css"/>
+<head>
+	<meta charset="UTF-8">
+	<title>formulaire contacte  </title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"
+</head>
+<body> <center>
+	<br><br>
+<h1> Bienvenue sur votre DASHBORD </h1>
+<button type='submit' class='btn btn-dark'>Crée un BOOK !</button></a>
+</center>
+</body>
+</html>
